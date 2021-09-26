667fbbd71f22b622da20022ef8406059b760f515
吕立青 <jimmy.jinglv@gmail.com>
2021-09-27 01:39:01 +0800

    update flomo_session cookie

--- pages/api/cors.js
+++ pages/api/cors.js
@@ -14,6 +14,20 @@ export default async function handler(req, res) {
   // Run cors
   await cors(req, res)
 
+  console.log(req.query)
+  console.log(req.headers.cookie)
+
+  try {
+    const response = await axios({
+      method: "get",
+      url: `https://flomoapp.com/api/memo`,
+      headers: req.query?.flomo_session ? { cookie: `flomo_session=${req.query?.flomo_session}` } : undefined,
+      withCredentials: true,
+    })
+    res.json(response)
+  } catch (error) {
+    res.json(error)
+  }
   // Rest of the API logic
-  res.json({ message: 'Hello Everyone!' })
+  // res.json({ message: 'Hello Everyone!' })
 }
